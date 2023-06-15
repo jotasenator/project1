b430bcd14aef81087447f853421b1f0c3cc17752
Maykel <jotasenator@gmail.com>
2023-06-15 17:22:16 -0400

    Installed markdown2 and convert to link all the entries and render the md content

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import markdown
 
 from . import util
 
@@ -7,4 +8,11 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def entry(request, entry):
+    with open(f"entries/{entry}.md") as f:
+        content = f.read()
+    html = markdown(content)
+    return render(request, "encyclopedia/entry.html", {"content": html})
 
